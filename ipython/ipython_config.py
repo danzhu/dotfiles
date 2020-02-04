4417cdf0a0cbb4ecdbf0833981be8225e4b118ca
Daniel Zhu <danzhu24@hotmail.com>
2020-02-04 00:55:59 -0500

    ipython: no confirm exit and disable vi mode prompt

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -2,4 +2,6 @@ from typing import Any, TYPE_CHECKING
 if TYPE_CHECKING:
     c: Any = None
 
+c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.prompt_includes_vi_mode = False
