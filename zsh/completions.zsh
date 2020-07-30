08986e455eb386adccfb8139112aeadfee8f441c
Daniel Zhu <danzhu24@hotmail.com>
2020-07-30 01:32:46 -0400

    zsh: add more ignore-line, set noclobber

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -7,7 +7,7 @@ zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]} r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' rehash true
 zstyle ':completion:*' ignore-parents parent pwd
-zstyle ':completion::*:(rm|rmdir|diff|vi|vim|emacs):*' ignore-line true
+zstyle ':completion::*:(rm|rmdir|chmod|chgrp|chown|diff|vi|vim|emacs):*' ignore-line true
 
 autoload -Uz compinit
 compinit
